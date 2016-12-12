4d9d5fb02958632240a6e5a56f1c76d72d65d9b1
Joe Seidel <seidel.jp@gmail.com>
2016-12-12 11:55:51 -0600

    changed celery req

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 import os
 #my comment here
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.wsgi import get_wsgi_application
