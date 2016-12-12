5842ce270ac6e6a0875a7eb2a22f38a4c4960405
Joe Seidel <seidel.jp@gmail.com>
2016-12-12 12:05:44 -0600

    updated gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 import os
 #my comment here
-DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
-sys.path.append(DJANGO_PATH)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.wsgi import get_wsgi_application
