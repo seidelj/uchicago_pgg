3a900a1afa65cb00a35d781a08c77d8d7bc677b2
Joe Seidel <seidel.jp@gmail.com>
2017-01-03 18:45:18 -0600

    cleaning up old files

--- Procfile
+++
@@ -1,4 +0,0 @@
-web: gunicorn wsgi --log-file -
-
-#NOTE: worker dyno is not enabled.
-worker: python manage.py celery worker --app=otree.celery.app:app --loglevel=INFO
--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, patterns, include
-
-urlpatterns = patterns('',
-	url(r'^', include('otree.default_urls')),	
-    url(r'^s3direct/', include('s3direct.urls')),
-)
